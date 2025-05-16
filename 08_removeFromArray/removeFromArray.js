a9bf6d2e13369006e5a08a2742f56a4f1d195c61
HollowGhoul16 <joshuavasquez425@outlook.com>
2025-05-16 17:46:56 -0400

    Finished exercises 6-11 from Arrays and Loops lesson!
    Some were interesting as you had to handle valid
    input, although they were all pretty simple.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rest) {
+    for(let i = 0; i < rest.length; i++) {
+        while(arr.some(element => element === rest[i])){
+            arr.splice(arr.indexOf(rest[i]), 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum = 0;
+    if(first < 0 || second < 0) return 'ERROR';
+    if(!Number.isInteger(first) || !Number.isInteger(second)) return 'ERROR';
 
+    if(first > second){
+        for(let i = second; i <= first; i++){
+        sum+=i;
+        }
+    } else {
+        for(let i = first; i <= second; i++){
+                sum+=i;
+            }
+    }
+    return sum;
 };
 
 // Do not edit below this line
